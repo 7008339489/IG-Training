419d2cc31799b8ff60e1af2d194ac7b4c42f6930
Pritish <pritish.kanha1234@gmail.com>
2023-08-01 21:34:59 +0530

    New Project Created

---
+++ IG_Training_SpringBoot/src/main/java/com/example/IG_Training_SpringBoot/IgTrainingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.IG_Training_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgTrainingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgTrainingSpringBootApplication.class, args);
+	}
+
+}
---
+++ IG_Training_SpringBoot/src/test/java/com/example/IG_Training_SpringBoot/IgTrainingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IG_Training_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgTrainingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
